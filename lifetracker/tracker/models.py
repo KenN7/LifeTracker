1a87b31b3da3ed2f79b2439987c2949ef2d511a1
Ken <ken@kenh.fr>
2017-04-11 17:46:58 +0200

    new ver, lets check mqtt

--- lifetracker/tracker/models.py
+++ lifetracker/tracker/models.py
@@ -11,25 +11,26 @@ class User(auth.models.User):
     
     class Meta:
         proxy = True
+        
+    pass
+    #def daily_events(self):
+        #today = datetime.datetime.today()
+        #return self.events.filter(date__year=today.year, date__month=today.month, date__day=today.day)
 
-    def daily_events(self):
-        today = datetime.datetime.today()
-        return self.events.filter(date__year=today.year, date__month=today.month, date__day=today.day)
+    #def tomorrow_events(self):
+        #today = datetime.datetime.today()
+        #return self.events.filter(date__year=today.year, date__month=today.month, date__day=today.day+1)
 
-    def tomorrow_events(self):
-        today = datetime.datetime.today()
-        return self.events.filter(date__year=today.year, date__month=today.month, date__day=today.day+1)
-
-    def later_events(self):
-        today = datetime.datetime.today()
-        return self.events.filter(date__gt=today)
+    #def later_events(self):
+        #today = datetime.datetime.today()
+        #return self.events.filter(date__gt=today)
 
 
-class Period(models.Model):
-    frequency = models.IntegerField()
+#class Period(models.Model):
+    #frequency = models.IntegerField()
 
-    def __str__(self):
-        return str(self.frequency)
+    #def __str__(self):
+        #return str(self.frequency)
 
 
 class Pad(models.Model):
@@ -40,30 +41,22 @@ class Pad(models.Model):
         return self.name
 
 
-class Icon(models.Model):
-    name = models.CharField(max_length=30)
-    path = models.CharField(max_length=50)
-
-    def __str__(self):
-        return self.name
-
-
-class Events(models.Model):
-    TYPES = (
-        ('Notif', 'Notification'),
-        ('Event', 'Event'),
-    )
-    name = models.CharField(max_length=50)
-    date = models.DateTimeField()
-    desc = models.TextField()
-    author = models.ForeignKey(User, related_name='events')
-    recurent = models.ForeignKey(Period, related_name='events')
-    icon = models.ForeignKey(Icon, related_name='events')
-    eventtype = models.CharField(max_length=20, choices=TYPES, default='Event')
+#class Events(models.Model):
+    #TYPES = (
+        #('Notif', 'Notification'),
+        #('Event', 'Event'),
+    #)
+    #name = models.CharField(max_length=50)
+    #date = models.DateTimeField()
+    #desc = models.TextField()
+    #author = models.ForeignKey(User, related_name='events')
+    #recurent = models.ForeignKey(Period, related_name='events')
+    #icon = models.ForeignKey(Icon, related_name='events')
+    #eventtype = models.CharField(max_length=20, choices=TYPES, default='Event')
 
 
-    def __str__(self):
-        return self.name
+    #def __str__(self):
+        #return self.name
 
 
 class Todo(models.Model):
@@ -74,4 +67,29 @@ class Todo(models.Model):
     def __str__(self):
         return self.name
 
+class Command(models.Model):
+    name = models.CharField(max_length=30)
+    command = models.CharField(max_length=50)
+    actusensor = models.ForeignKey(Sensor, related_name='sensor')
+    
+    def __str__(self):
+        return self.name
+    
+class Data(models.Model):
+    name = models.CharField(max_length=30)
+    value = models.CharField(max_length=50)
+    actusensor = models.ForeignKey(Sensor, related_name='data')
+    
+    def __str__(self):
+        return self.name
+
+class Actusensor(models.Model):
+    name = models.CharField(max_length=30)
+    owner = models.ForeignKey(User, related_name='actusensor')
+    apikey = models.CharField(max_length=30)
+    address = models.CharField(max_length=30)
+    image = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
 
